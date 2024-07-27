b282efc3778179ef86b6db1c425fdf52705b9f44
Revan-coder <revanhesenov802@gmail.com>
2024-07-27 12:29:05 +0400

    Add initial commit

---
+++ Lesson/24.07.2024/InitialConselApp/InitialConselApp/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace InitialConselApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {   // PascalCase
+            // canelCase - variables;
+
+            // Number section
+            long bignumber = 1000000;
+            int userAge = 300000;
+            short id = 2000;
+            byte num = 5;
+
+
+
+            ulong bigNumber2 = -2;
+
+
+
+
+
+            // Floating
+            float floatType1 = 5.3f;
+            double doubleType = 15.2;
+            decimal decimalType = 25.2m;
+
+
+            // Logical
+            bool boolType = true;
+
+            // Char
+            char charType = 'a'; // sadece tek simvol
+
+            // String
+            string name = "Revan"; // qosa dırnaq
+
+            // DateTime
+            DateTime dateTime = new DateTime(2024, 7, 19,);
+
+              
+            // Car
+            string carName = "BMW";
+            string carCity = "Baki";
+            string carModel = "X5";
+            bool carNew = false;
+            float carEngine = 3.0f;
+
+
+            // Person
+            string personName = "Revan";
+            string personEmail = "revanhesenov802@gmail.com";
+            byte personAge = 19;
+            decimal personHeight = 1.75m;
+            bool personBoy = true;
+            string personAddress = "huseyin seyidzade 32";
+            
+            
+
+
+
+            
+        }
+    }
+}
---
+++ Lesson/24.07.2024/InitialConselApp/SecondConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SecondConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 6;
+            int num2 = 7;
+
+            int sum = 0;
+
+            sum = num1 * num2;
+             
+            Console.WriteLine(sum);
+        }
+    }
+}
